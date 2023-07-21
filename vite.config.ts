5acb653eac4aad7f43bb87d0e69c92c93f7f1cc1
Claquettes <math@claq.fr>
2023-07-21 15:36:34 +0200

    ci.yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/BlogV2/',
+  build: {
+    outDir: 'dist', // Output directory for the production build
+    emptyOutDir: true, // Clean the output directory before each build
+  },
+});
