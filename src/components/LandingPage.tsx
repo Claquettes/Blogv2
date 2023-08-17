89ed8e9cfbc1b2c09513606f8763aebed36f3811
Claquettes <math@claq.fr>
2023-08-17 21:40:07 +0200

    created color palette file, and imported it into the index.css for global styling

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,9 +1,47 @@
-//we import the css file
-
+import React, { useState, useEffect } from "react";
 import "./styles/LandingPage.css";
 
 function LandingPageComponent() {
-  return <div className="entire-about-me-compo">Mathieu Ponton</div>;
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [direction, setDirection] = useState({ x: 1, y: 1 });
+
+  useEffect(() => {
+    const animationInterval = setInterval(() => {
+      setPosition((prevPosition) => {
+        let newX = prevPosition.x + direction.x;
+        let newY = prevPosition.y + direction.y;
+
+        if (newX <= 0 || newX >= window.innerWidth - 200) {
+          setDirection((prevDirection) => ({
+            ...prevDirection,
+            x: -prevDirection.x,
+          }));
+        }
+
+        if (newY <= 0 || newY >= window.innerHeight - 50) {
+          setDirection((prevDirection) => ({
+            ...prevDirection,
+            y: -prevDirection.y,
+          }));
+        }
+
+        return { x: newX, y: newY };
+      });
+    }, 10);
+
+    return () => clearInterval(animationInterval);
+  }, [direction]);
+
+  return (
+    <div className="LandingPage">
+      <div
+        className="Name-center"
+        style={{ left: position.x, top: position.y }}
+      >
+        <h1>Mathieu Ponton</h1>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPageComponent;
