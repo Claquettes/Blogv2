831e7082a695720f5912629c0ae7494f57e6023c
Claquettes <math@claq.fr>
2023-08-18 21:57:45 +0200

    added sass, started to do prework for an animation for the landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,44 +1,44 @@
 import React, { useState, useEffect } from "react";
-import "./styles/LandingPage.css";
+import "./styles/LandingPage.scss";
 
 function LandingPageComponent() {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [direction, setDirection] = useState({ x: 1, y: 1 });
-
+  //once the component is mounted, we want to start the animation
   useEffect(() => {
-    const animationInterval = setInterval(() => {
-      setPosition((prevPosition) => {
-        let newX = prevPosition.x + direction.x;
-        let newY = prevPosition.y + direction.y;
-
-        if (newX <= 0 || newX >= window.innerWidth - 200) {
-          setDirection((prevDirection) => ({
-            ...prevDirection,
-            x: -prevDirection.x,
-          }));
-        }
-
-        if (newY <= 0 || newY >= window.innerHeight - 50) {
-          setDirection((prevDirection) => ({
-            ...prevDirection,
-            y: -prevDirection.y,
-          }));
-        }
-
-        return { x: newX, y: newY };
-      });
-    }, 10);
-
-    return () => clearInterval(animationInterval);
-  }, [direction]);
+    //get all the letters without using querySelectorAll (WITHOUT JQUERY) (jsquery is banned from this project)
+    const letters = document.getElementsByClassName("letter");
+    //get the number of letters
+    const numberOfLetters = letters.length;
+    //for each letter, we want to add a class that will trigger the animation
+    for (let i = 0; i < numberOfLetters; i++) {
+      console.log(letters[i]);
+      if (i % 2 === 0) {
+        letters[i].classList.add("letter-animation");
+      } else {
+        letters[i].classList.add("letter-animation-reverse");
+      }
+    }
+  }, []);
 
   return (
     <div className="LandingPage">
-      <div
-        className="Name-center"
-        style={{ left: position.x, top: position.y }}
-      >
-        <h1>Mathieu Ponton</h1>
+      <div className="Name-center">
+        <div className="letters-containers">
+          <div className="letter">H</div>
+          <div className="letter">i</div>
+          <div className="letter">,</div>
+          <div className="letter">I</div>
+          <div className="letter">'</div>
+          <div className="letter">m</div>
+          <div className="letter">&nbsp;</div>
+          <div className="letter">M</div>
+          <div className="letter">a</div>
+          <div className="letter">t</div>
+          <div className="letter">h</div>
+          <div className="letter">i</div>
+          <div className="letter">e</div>
+          <div className="letter">u</div>
+          <div className="letter">_</div>
+        </div>
       </div>
     </div>
   );
