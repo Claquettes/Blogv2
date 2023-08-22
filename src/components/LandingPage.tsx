1ce72e7844783e434594c7b78fc487ecb3be073d
Claquettes <math@claq.fr>
2023-08-22 02:39:12 +0200

    s

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./styles/LandingPage.scss";
 import AboutMeComponent from "./AboutMe";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
 import App from "../App";
 
 function LandingPageComponent() {
+  const ref = useRef();
   useEffect(() => {
     const letters = document.getElementsByClassName("letter");
     //get the number of letters
@@ -27,35 +30,44 @@ function LandingPageComponent() {
 
   return (
     <div>
-      <section className="LandingPage">
-        <div className="LandingPage">
-          <div className="Name-center">
-            <div className="letters-containers">
-              <div className="letter">H</div>
-              <div className="letter">i</div>
-              <div className="letter">_</div>
-              <div className="letter">I</div>
-              <div className="letter">'</div>
-              <div className="letter">m</div>
-              <div className="letter">&nbsp;</div>
-              <div className="letter">M</div>
-              <div className="letter">a</div>
-              <div className="letter">t</div>
-              <div className="letter">h</div>
-              <div className="letter">i</div>
-              <div className="letter">e</div>
-              <div className="letter">u</div>
-              <div className="letter">_</div>
+      <Parallax pages={3} ref={ref}>
+        <ParallaxLayer offset={0} speed={0.5} factor={0.2}>
+          <section className="LandingPage">
+            <div className="LandingPage">
+              <h3> Hei</h3>
+              <div className="Name-center">
+                <div className="letters-containers">
+                  <div className="letter">H</div>
+                  <div className="letter">i</div>
+                  <div className="letter">_</div>
+                  <div className="letter">I</div>
+                  <div className="letter">'</div>
+                  <div className="letter">m</div>
+                  <div className="letter">&nbsp;</div>
+                  <div className="letter">M</div>
+                  <div className="letter">a</div>
+                  <div className="letter">t</div>
+                  <div className="letter">h</div>
+                  <div className="letter">i</div>
+                  <div className="letter">e</div>
+                  <div className="letter">u</div>
+                  <div className="letter">_</div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </section>
-      <section className="about-me" id="aboutme">
-        <AboutMeComponent />
-      </section>
-      <section className="projects" id="projects">
-        <App />
-      </section>
+          </section>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.5} factor={1}>
+          <section className="about-me" id="aboutme">
+            <AboutMeComponent />
+          </section>
+        </ParallaxLayer>
+        <ParallaxLayer offset={2} speed={0.5} factor={1}>
+          <section className="projects" id="projects">
+            <App />
+          </section>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
